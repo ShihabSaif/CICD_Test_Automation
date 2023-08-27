fd045a6be7c392b4be82fe7a8856a441ab7787da
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2023-08-28 00:11:18 +0600

    automated jira issue cration added

--- src/test/java/in/at/testanotation/TestClass.java
+++ src/test/java/in/at/testanotation/TestClass.java
@@ -1,10 +1,14 @@
 package in.at.testanotation;
 
+import net.rcarz.jiraclient.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class TestClass {
@@ -33,5 +37,23 @@ public class TestClass {
     public void Test_03() throws InterruptedException {
         driver.navigate().to("https://www.prothomalo.com/");
         System.out.println("test 3 is : " + driver.getTitle());
+        Assert.assertEquals("dummy text", driver.getTitle());
+    }
+
+    @AfterTest
+    public void quit(ITestResult result) throws JiraException {
+        driver.quit();
+
+        if(result.getStatus() ==ITestResult.FAILURE)
+        {
+            BasicCredentials cred = new BasicCredentials("shihab_qa", "Sh!@Ud!nP$l2022P$L");
+            JiraClient jira = new JiraClient("https://jira.surecashbd.com/secure/Dashboard.jspa", cred);
+            Issue issueName = jira.createIssue("TK", "Bug")
+                 .field(Field.SUMMARY, result.getMethod().getMethodName() + "is failed due to" + result.getThrowable().toString())
+                 .field(Field.DESCRIPTION, "get the description").execute();
+
+            System.out.println("issue created in jira with : " + issueName.getKey());
+
+        }
     }
 }
